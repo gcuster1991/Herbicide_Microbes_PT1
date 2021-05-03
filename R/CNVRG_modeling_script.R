9dd48ef843c207ef2b1a7d69cc295682573c02c1
J. G. Harrison <jgharrison1982@gmail.com>
2021-05-03 11:34:45 -0600

    first commit

---
+++ R/CNVRG_modeling_script.R
@@ -0,0 +1,52 @@
+rm(list=ls())
+set.seed(666)
+#install.packages("CNVRG")
+library(rstan)
+library(CNVRG)
+
+#This takes STDIN so we call this script via the command line for different files
+#We pass in a file to STDIN. Rscript CNVRG_modedling_script.R yourfile
+#inargs[1] is then the path to your file. 
+
+inargs <- commandArgs(trailingOnly = TRUE)
+
+dat <- read.csv(inargs[1], 
+                fill = T, header = T, stringsAsFactors = F)
+
+#Order by treatment
+dat <- dat[order(dat$treatment),]
+
+#check for zero counts and rows with no data, then add one to all data
+table(rowSums(dat[,3:length(dat)]) > 0)
+table(colSums(dat[,3:length(dat)]) > 0)
+#Some OTUs are not present in these data. Remove them. 
+dat2 <- dat[, c(TRUE, TRUE, colSums(dat[,3:length(dat)]) > 0)]
+
+#check
+table(colSums(dat2[,3:length(dat2)]) > 0 )
+dat2[1:3,1:5]
+
+#For ease, extracting treatment vector
+treatments <- dat2$treatment
+#remove treatment vector from dataframe
+dat2 <- dat2[,-2]
+
+dat2[,2:length(dat2)] <-  1 + dat2[,2:length(dat2)]
+
+#Commented out options are useful for cnvrg_HMC
+modelOut <- cnvrg_VI(
+  countData = dat2,
+  starts = indexer(treatments)$starts,
+  ends = indexer(treatments)$ends,
+  #algorithm = "NUTS",
+  #chains = 2,
+  #burn = 500,
+  #samples = 1500,
+  #thinning_rate = 2,
+  output_samples = 250,
+  #  cores = 16,
+  params_to_save = c("pi","p")
+)
+save.image(file = paste(inargs[1], ".Rdata", sep = ""))
+
+
--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 This repo contains scripts for processing of Herb PT1 enzyme and sequence data. 
 
 Collaborators: Josh Harrison, Andrew Kniss, and Linda van Diepen
+
+Descriptions of repo contents:
+
+forModeling_*_otuTables -- Directories containing OTU tables for 16s and ITS data that have been formatted for CNVRG modeling.
+
