9d4b571adc9459ede04d3bb6451f0ff51e3c3b54
Josh Harrison <jharrison@mac.uwyo.edu>
2021-07-23 16:57:58 -0600

    new cnvrg is simpler so redoing

--- R/differential_rel_abund.R
+++ R/differential_rel_abund.R
@@ -11,12 +11,5 @@ summary_by_group <- data.frame(matrix(nrow = 20, ncol = 2))
 for(i in 1:length(inputs)){
   load(inputs[i])
   diffs <- diff_abund(model_out = modelOut, countData = dat3)
-  write.csv(diffs, file = paste(gsub("Rdata","",inputs[i]), "_diff_estimates.csv", sep = ""), row.names = F)
-  for(j in 1:length(diffs[,1])){
-    summary_by_group[j,2] <- length(which(diffs[j,2:length(diffs)] > 0.95 | diffs[j,2:length(diffs)] < 0.05))
-  }
-  summary_by_group[,1] <- diffs[,1]
-  names(summary_by_group) <- c("comparison", "num_taxa")
-  write.csv(summary_by_group, file = paste(gsub("Rdata","",inputs[i]), "_diff_Summary.csv", sep = ""), row.names = F)
-  rm(modelOut, dat3)
+  save(diffs, file = paste(inputs[i], "_diff_estimates.Rdata", sep = ""))
 }
